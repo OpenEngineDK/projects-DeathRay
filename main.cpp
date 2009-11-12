a038c63687f84142e2167683036930d1d5ad950f
asgerhoedt <asgerhoedt@gmail.com>
2009-11-12 12:54:54 +0000

    Added enough to main to render

--- main.cpp
+++ main.cpp
@@ -12,12 +12,23 @@
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
 #include <Core/Engine.h>
+#include <Renderers/OpenGL/Renderer.h>
+#include <Display/Camera.h>
 
 // SimpleSetup
 #include <Utils/SimpleSetup.h>
+#include <Display/SDLEnvironment.h>
+#include <Display/Viewport.h>
 
-// Game factory
-//#include "GameFactory.h"
+// *** Death rays ***
+
+// Resources
+#include <Resources/ResourceManager.h>
+#include <Resources/MHDResource.h>
+
+// Scene and rendering
+#include <Renderers/OpenGL/DoseCalcRenderingView.h>
+#include <Scene/DoseCalcNode.h>
 
 // name spaces that we will be using.
 // this combined with the above imports is almost the same as
@@ -25,6 +36,10 @@
 using namespace OpenEngine::Logging;
 using namespace OpenEngine::Core;
 using namespace OpenEngine::Utils;
+using namespace OpenEngine::Scene;
+using namespace OpenEngine::Display;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Renderers::OpenGL;
 
 /**
  * Main method for the first quarter project of CGD.
@@ -33,14 +48,32 @@ using namespace OpenEngine::Utils;
  * method in Java.
  */
 int main(int argc, char** argv) {
-    // Setup logging facilities.
-    Logger::AddLogger(new StreamLogger(&std::cout));
-
-    // Print usage info.
-    logger.info << "========= Running OpenEngine Test Project =========" << logger.end;
+    // Create Viewport and renderingview
+    IEnvironment* env = new SDLEnvironment(800,600);
+    Viewport* vp = new Viewport(env->GetFrame());
+    IRenderingView* rv = new DoseCalcRenderingView(*vp);
 
     // Create simple setup
-    SimpleSetup* setup = new SimpleSetup("Example Project Title");
+    SimpleSetup* setup = new SimpleSetup("Death by tray.. I mean Death Ray", vp, env, rv);
+
+    // Setup Loaders
+    ResourceManager<ITexture3DResource>::AddPlugin(new MHDResourcePlugin());
+    DirectoryManager::AppendPath("projects/DeathRay/data/");    
+
+    // Setup Camera
+    Camera* cam = setup->GetCamera();
+    cam->SetPosition(Vector<3, float>(-256.0, 200.0, -256.0));
+    cam->LookAt(0.0, 127.0, 0.0);
+    
+    // Setup the scene
+    ITexture3DResourcePtr mhd = ResourceManager<ITexture3DResource>::Create("20-P.mhd");
+    DoseCalcNode* doseNode = new DoseCalcNode(mhd);
+    setup->GetRenderer().InitializeEvent().Attach(*doseNode);
+
+    // setup the scene graph
+    ISceneNode* root = setup->GetScene();
+    root->AddNode(doseNode);
+    
     // Start the engine.
     setup->GetEngine().Start();
 
